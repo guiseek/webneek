50327c99346e26204587af50ea0c3b808505aeea
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-11-21 01:33:35 -0300

    chore(repo): tool para iniciar tarefa
    prompt de perguntas criado para facilitar inicio de tarefa

---
+++ tools/init.ts
@@ -0,0 +1,44 @@
+import { Listr } from 'listr2';
+import { prompt } from 'enquirer';
+import { types } from '../.cz-config';
+import { createTask } from './utils/create-task';
+
+interface InitPromp {
+  type: string;
+  name: string;
+  confirm?: boolean;
+}
+
+prompt<InitPromp>([
+  {
+    type: 'select',
+    name: 'type',
+    message: 'Onde sua task melhor se encaixa?',
+    initial: 0,
+    choices: types.map(({ value, name }) => ({ name: value, message: name })),
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Qual o nome da tarefa?',
+  },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Podemos começar?',
+  },
+]).then(async ({ type, name, confirm }) => {
+  if (confirm) {
+    const clean = (str: string) => str.replace(/[^A-Z0-9]+/gi, '-');
+    const branch = (arr: string[]) =>
+      arr.map((it) => it.toLocaleLowerCase()).join('/');
+    const tasks = new Listr([
+      createTask('Build', 'git', [
+        'checkout',
+        '-b',
+        branch([type, clean(name)]),
+      ]),
+    ]);
+    await tasks.run().catch(({ message }) => console.error(message));
+  }
+});
