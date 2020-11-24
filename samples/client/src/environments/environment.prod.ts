f0b633337035ed9b181cf1ce3ca46f6f1353d364
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-11-24 03:43:57 -0300

    feat(client): exemplo de chat SDP passo a passo

--- samples/client/src/environments/environment.prod.ts
+++ samples/client/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import { WebSocketConfig } from '../app/shared';
+
 export const environment = {
   production: true,
+};
+
+export const webSocketConfig: WebSocketConfig = {
+  url: 'https://webneek-server.herokuapp.com',
+  options: {},
 };
